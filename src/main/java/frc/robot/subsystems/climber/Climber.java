d2d49e17cad73bba79bdfd58c38cc57f01ece4b8
shyyla <shyrajavong10@gmail.com>
2026-02-18 16:55:22 -0500

    added linear acuators to intake + squential command for climber

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -7,76 +7,77 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase{
-    private final Elevator elevator = new Elevator();
+    private final Elevator innerElevator = new Elevator();
+
 
     public Climber() {
         SmartDashboard.putData(this);
     }
 
     public Command setClimberL1() {
-        var cmd = elevator.setHeightCommand(ClimberConfig.ElevatorSetpoints.L1);
-        cmd.addRequirements(elevator);
+        var cmd = innerElevator.setHeightCommand(ClimberConfig.ElevatorSetpoints.L1);
+        cmd.addRequirements(innerElevator);
         return cmd.withName("SetElevatorL1");
     }
 
     public Command setClimberL2() {
-        var cmd = elevator.setHeightCommand(ClimberConfig.ElevatorSetpoints.L2);
-        cmd.addRequirements(elevator);
+        var cmd = innerElevator.setHeightCommand(ClimberConfig.ElevatorSetpoints.L2);
+        cmd.addRequirements(innerElevator);
         return cmd.withName("SetElevatorL2");
     }
 
     public Command setClimberL3() {
-        var cmd = elevator.setHeightCommand(ClimberConfig.ElevatorSetpoints.L3);
-        cmd.addRequirements(elevator);
+        var cmd = innerElevator.setHeightCommand(ClimberConfig.ElevatorSetpoints.L3);
+        cmd.addRequirements(innerElevator);
         return cmd.withName("SetElevatorL3");
     }
 
     public Command setClimberStow() {
-        var cmd = elevator.setHeightCommand(ClimberConfig.ElevatorSetpoints.STOW);
-        cmd.addRequirements(elevator);
+        var cmd = innerElevator.setHeightCommand(ClimberConfig.ElevatorSetpoints.STOW);
+        cmd.addRequirements(innerElevator);
         return cmd.withName("SetElevatorStow");
     }
     
     public Command deployOutsideHooks() {
-        var cmd = elevator.deployOuterHooksCommand();
-        cmd.addRequirements(elevator);
+        var cmd = innerElevator.deployOuterHooksCommand();
+        cmd.addRequirements(innerElevator);
         return cmd.withName("deployOuterHooks");
     }
 
     public Command deployInsideHook() {
-        var cmd = elevator.deployInnerHookCommand();
-        cmd.addRequirements(elevator);
+        var cmd = innerElevator.deployInnerHookCommand();
+        cmd.addRequirements(innerElevator);
         return cmd.withName("deployInnerHooks");
     }
 
     public Command retractOutsideHooks() {
-        var cmd = elevator.retractOuterHooksCommand();
-        cmd.addRequirements(elevator);
+        var cmd = innerElevator.retractOuterHooksCommand();
+        cmd.addRequirements(innerElevator);
         return cmd.withName("retractOuterHooks");
     }
 
     public Command retractInsideHooks() {
-        var cmd = elevator.retractInnerHookCommand();
-        cmd.addRequirements(elevator);
+        var cmd = innerElevator.retractInnerHookCommand();
+        cmd.addRequirements(innerElevator);
         return cmd.withName("retractInnerHooks");
     }
 
     public Command stowOutsideHooks() {
-        var cmd = elevator.retractOuterHooksCommand();
-        cmd.addRequirements(elevator);
+        var cmd = innerElevator.retractOuterHooksCommand();
+        cmd.addRequirements(innerElevator);
         return cmd.withName("retractOuterHooks");
     }
 
     public Command stowAllHooks() {
-        var cmd = elevator.StowAllHooksCommand();
-        cmd.addRequirements(elevator);
+        var cmd = innerElevator.StowAllHooksCommand();
+        cmd.addRequirements(innerElevator);
         return cmd.withName("retractInnerHooks");
     }
 
-    //public Command goToL1(){
-    //var cmd = retractOutsideHooks().andThen(setClimberL1()).andThen(deployOutsideHooks());
-     //return cmd.withName("goToL1");
-    //}
+    public Command goToL1(){
+    var cmd = retractOutsideHooks().andThen(setClimberL1()).andThen(deployOutsideHooks()).andThen(setClimberL2()).andThen(retractInsideHooks());
+     return cmd.withName("goToL1"); 
+    }
 
     //we should have a prepare climb thing and an autoclimb, look into the elevator for how to do autoclimb
     
